f2f9a3cc648eddab3e83d7bd31b811a51137e385
Vineeth Ramesh <67580974+vineeth2281@users.noreply.github.com>
2026-06-02 11:00:45 +0530

    Update core.py

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -2152,3 +2152,7 @@ def get_user_agent(database: Database, source: QuerySource | None) -> str:
         return user_agent_func(database, source)
 
     return DEFAULT_USER_AGENT
+
+
+def linterrortest: 
+return unwanter_variable
